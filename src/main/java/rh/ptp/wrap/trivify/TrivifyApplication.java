f2452ee4976d4a2cff3a84d30879dc1fc09ac738
Paul <paulachenbach1363@gmail.com>
2025-03-28 16:55:07 +0100

    initialsetup

---
+++ src/main/java/rh/ptp/wrap/trivify/TrivifyApplication.java
@@ -0,0 +1,13 @@
+package rh.ptp.wrap.trivify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrivifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrivifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rh/ptp/wrap/trivify/TrivifyApplicationTests.java
@@ -0,0 +1,13 @@
+package rh.ptp.wrap.trivify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrivifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
