dba74686067a7909bfe0693f612ea8fad04aaa44
Robin Wagner <82574602+Sommer2019@users.noreply.github.com>
2025-05-19 18:23:51 +0200

    feat: update build script to use TAG for frontend and backend Docker images

--- build.bat
+++ build.bat
@@ -20,15 +20,17 @@ if /I "%BRANCH_SAFE%"=="main" (
 echo On branch "%BRANCH%", tagging images as :"%TAG%".
 REM ==== FRONTEND ====
 echo Building frontend...
-docker build --build-arg REACT_APP_API_URL=http://127.0.0.1:9090/api -t quiz-frontend ./frontend
+docker build --build-arg REACT_APP_API_URL=http://127.0.0.1:9090/api -t trivify-frontend:%TAG% ./frontend
 if %errorlevel% neq 0 goto :error
 
 echo Saving frontend image...
 docker save -o trivify-frontend.tar trivify-frontend:%TAG%
+if %errorlevel% neq 0 goto :error
 
 echo Tagging and pushing frontend...
-docker tag trivify-frontend lesommer2019/trivify-frontend:%TAG%
+docker tag trivify-frontend:%TAG% lesommer2019/trivify-frontend:%TAG%
 docker push lesommer2019/trivify-frontend:%TAG%
+if %errorlevel% neq 0 goto :error
 
 REM ==== BACKEND ====
 echo Building backend...
@@ -37,15 +39,17 @@ call mvnw clean package
 if %errorlevel% neq 0 goto :error
 cd ..
 
-docker build -t trivify-backend:beta ./backend
+docker build -t trivify-backend:%TAG% ./backend
 if %errorlevel% neq 0 goto :error
 
 echo Saving backend image...
 docker save -o trivify-backend.tar trivify-backend:%TAG%
+if %errorlevel% neq 0 goto :error
 
 echo Tagging and pushing backend...
-docker tag trivify-backend lesommer2019/trivify-backend:%TAG%
+docker tag trivify-backend:%TAG% lesommer2019/trivify-backend:%TAG%
 docker push lesommer2019/trivify-backend:%TAG%
+if %errorlevel% neq 0 goto :error
 
 echo All done!
 goto :eof
