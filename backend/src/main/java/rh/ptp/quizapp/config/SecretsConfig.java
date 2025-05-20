c7c17047c8bcbabcb4b849678a5101b78fd16315
Robin Wagner <robin.wagner@axa.de>
2025-05-20 07:51:03 +0200

    Remove reCAPTCHA configuration and related dependencies from application.yml, pom.xml, and SecretsConfig.java

--- backend/src/main/java/rh/ptp/quizapp/config/SecretsConfig.java
+++ backend/src/main/java/rh/ptp/quizapp/config/SecretsConfig.java
@@ -1,100 +1,19 @@
 package rh.ptp.quizapp.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Data
 @ConfigurationProperties(prefix = "app")
 public class SecretsConfig {
     private String jwtSecret;
     private long jwtExpirationInMs;
-    private String recaptchaSecretKey;
-    private String recaptchaSiteKey;
     private String mailUsername;
     private String mailPassword;
     private String mailHost;
     private int mailPort;
     private String frontendUrl;
     private String adminPassword;
-
-    // Getters and Setters
-    public String getJwtSecret() {
-        return jwtSecret;
-    }
-
-    public void setJwtSecret(String jwtSecret) {
-        this.jwtSecret = jwtSecret;
-    }
-
-    public long getJwtExpirationInMs() {
-        return jwtExpirationInMs;
-    }
-
-    public void setJwtExpirationInMs(long jwtExpirationInMs) {
-        this.jwtExpirationInMs = jwtExpirationInMs;
-    }
-
-    public String getRecaptchaSecretKey() {
-        return recaptchaSecretKey;
-    }
-
-    public void setRecaptchaSecretKey(String recaptchaSecretKey) {
-        this.recaptchaSecretKey = recaptchaSecretKey;
-    }
-
-    public String getRecaptchaSiteKey() {
-        return recaptchaSiteKey;
-    }
-
-    public void setRecaptchaSiteKey(String recaptchaSiteKey) {
-        this.recaptchaSiteKey = recaptchaSiteKey;
-    }
-
-    public String getMailUsername() {
-        return mailUsername;
-    }
-
-    public void setMailUsername(String mailUsername) {
-        this.mailUsername = mailUsername;
-    }
-
-    public String getMailPassword() {
-        return mailPassword;
-    }
-
-    public void setMailPassword(String mailPassword) {
-        this.mailPassword = mailPassword;
-    }
-
-    public String getMailHost() {
-        return mailHost;
-    }
-
-    public void setMailHost(String mailHost) {
-        this.mailHost = mailHost;
-    }
-
-    public int getMailPort() {
-        return mailPort;
-    }
-
-    public void setMailPort(int mailPort) {
-        this.mailPort = mailPort;
-    }
-
-    public String getFrontendUrl() {
-        return frontendUrl;
-    }
-
-    public void setFrontendUrl(String frontendUrl) {
-        this.frontendUrl = frontendUrl;
-    }
-
-    public String getAdminPassword() {
-        return adminPassword;
-    }
-
-    public void setAdminPassword(String adminPassword) {
-        this.adminPassword = adminPassword;
-    }
-} 
+}
