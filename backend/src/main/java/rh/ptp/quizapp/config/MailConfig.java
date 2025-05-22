80500bc09600d14d274596498e7476901c11593d
Robin Wagner <82574602+Sommer2019@users.noreply.github.com>
2025-05-22 14:19:55 +0200

    fix: update import statement for Value annotation in MailConfig

--- backend/src/main/java/rh/ptp/quizapp/config/MailConfig.java
+++ backend/src/main/java/rh/ptp/quizapp/config/MailConfig.java
@@ -1,7 +1,7 @@
 package rh.ptp.quizapp.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
