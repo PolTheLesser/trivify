302021fb34fd42908acef73c6f253c8cb73b4cdb
Robin Wagner <82574602+Sommer2019@users.noreply.github.com>
2025-05-24 12:05:06 +0200

    feat: add NoSuchElementException handler and refactor entities to use Lombok's @Data

--- backend/src/main/java/rh/ptp/quizapp/model/QuizResult.java
+++ backend/src/main/java/rh/ptp/quizapp/model/QuizResult.java
@@ -1,10 +1,13 @@
 package rh.ptp.quizapp.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "quiz_results")
+@Data
 public class QuizResult {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,55 +29,6 @@ public class QuizResult {
 
     @Column(nullable = false)
     private LocalDateTime playedAt;
-
-    // Getters und Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Quiz getQuiz() {
-        return quiz;
-    }
-
-    public void setQuiz(Quiz quiz) {
-        this.quiz = quiz;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-
-    public int getMaxPossibleScore() {
-        return maxPossibleScore;
-    }
-
-    public void setMaxPossibleScore(int maxPossibleScore) {
-        this.maxPossibleScore = maxPossibleScore;
-    }
-
-    public LocalDateTime getPlayedAt() {
-        return playedAt;
-    }
-
-    public void setPlayedAt(LocalDateTime playedAt) {
-        this.playedAt = playedAt;
-    }
 
     @PrePersist
     protected void onCreate() {
