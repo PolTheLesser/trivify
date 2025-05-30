10635c350bc42f3ad7b3dbc5ab5318297d4d6bce
Robin Wagner <82574602+Sommer2019@users.noreply.github.com>
2025-05-30 11:01:45 +0200

    chore: wrap application in React.StrictMode for improved performance and debugging

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,21 @@
-import { AuthProvider } from "./contexts/AuthContext";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import "./index.css";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthProvider } from "./contexts/AuthContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import "./index.css";
 import { register } from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-register();
 
-
-ReactDOM.render(
-    <AuthProvider>
-        <ThemeProvider>
-            <App />
-        </ThemeProvider>
-    </AuthProvider>,
-    document.getElementById("root")
+root.render(
+    <React.StrictMode>
+        <AuthProvider>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </AuthProvider>
+    </React.StrictMode>
 );
+
+register();
