eea75f35e7c3b82abd3f79862eda0b7ce69594fb
Paul <paulachenbach1363@gmail.com>
2025-06-03 19:59:18 +0200

    feat: add more comments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,16 @@ import {ThemeProvider} from "./contexts/ThemeContext";
 import "./index.css";
 import {register} from './serviceWorkerRegistration';
 
+/** index.js
+ Einstiegspunkt der React-Anwendung. Diese Datei rendert die Haupt-App-Komponente und
+ bindet globale Kontexte wie Authentifizierung und Theme-Management ein.
+
+ Funktionalitäten:
+ - Rendert die `App`-Komponente in das HTML-Element mit der ID `root`
+ - Umgibt die gesamte App mit dem `AuthProvider` zur Verwaltung des Benutzerzustands
+ - Integriert den `ThemeProvider` zur dynamischen Umschaltung zwischen Light- und Dark-Mode
+ - Lädt globale CSS-Definitionen über `index.css`
+ */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
